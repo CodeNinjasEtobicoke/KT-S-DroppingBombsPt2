8c535d84f1ba9c4eb5888e56c3a0b4146c84f35a
AstroBoy2013 <astrobrandon2013@gmail.com>
2025-03-11 16:52:54 -0400

    on game 4 step 3

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
+
 
 
 public class GameManager : MonoBehaviour
@@ -9,19 +10,24 @@ public class GameManager : MonoBehaviour
     private Spawner spawner;
     public GameObject title;
     private Vector2 screenBounds;
+    
+    [Header("Player")]
     public GameObject playerPrefab;
     private GameObject player;
     private bool gameStarted = false;
     public GameObject splash;
 
+    [Header("Score")]
+    public TMP_Text scoreText;
+    public int pointsWorth = 1;
+    public int score;
 
 
-    
-    void Awake()
+    private void Awake()
     {
         spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
-        player = playerPrefab;
+        scoreText.enabled = false;
     }
 
     // Start is called before the first frame update
@@ -36,13 +42,20 @@ public class GameManager : MonoBehaviour
     {
         if (!gameStarted)
         {
-            if(Input.anyKeyDown)
+            if (Input.anyKeyDown)
             {
                 ResetGame();
-
             }
         }
-        
+        else
+        {
+            if (!player)
+            {
+                OnPlayerKilled();
+            }
+        }
+
+
 
         var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
 
@@ -50,15 +63,34 @@ public class GameManager : MonoBehaviour
         {
             if (bombObject.transform.position.y < (-screenBounds.y - 12))
             {
+                if (gameStarted)
+                {
+                    score += pointsWorth;
+                    scoreText.text = "Score:" + score.ToString();
+
+                }
                 Destroy(bombObject);
             }
         }
     }
+    void OnPlayerKilled()
+    {
+        spawner.active = false;
+        gameStarted = false;
+        splash.SetActive(true);
+    }
+
     void ResetGame()
-        {
+    {
         spawner.active = true;
         title.SetActive(false);
         player = Instantiate(playerPrefab, new Vector3(0, 0, 0), playerPrefab.transform.rotation);
         gameStarted = true;
+        splash.SetActive(false);
+
+        scoreText.enabled = true;
+        score = 0;
+
+        scoreText.text = "Score: " + score.ToString();
     }
 }
